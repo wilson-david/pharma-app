525ebc64f0d3903b0fe19d91a4e44d7afcbb86a8
wilson-david <49764177+wilson-david@users.noreply.github.com>
2024-03-25 19:34:19 -0500

    interfaz del login

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import * as Svg from 'react-native-svg';
 
 export default function App() {
+
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! prueba 222</Text>
+      <Text style={styles.titulo}>Hello</Text>
+      <Text style={styles.subTitle}>Sing In to  your account</Text>
+      <TextInput style={styles.textInput} placeholder='example@gmail.com'></TextInput>
+      <TextInput style={styles.textInput} placeholder='Password'></TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo:{
+    fontSize: 80,
+    color: '#000',
+    fontWeight: 'bold'
+  },
+
+  subTitle:{
+    fontSize: 30,
+    color: 'gray'
+  },
+
+  textInput:{
+    borderWidth: 1,
+    borderColor: 'gray',
+    paddingStart: 30,
+    padding: 10,
+    width: '80%',
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#fff'
+  }
 });
